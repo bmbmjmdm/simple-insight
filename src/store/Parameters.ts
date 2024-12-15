48c523e2e298a4dad72329d498c2f759a1945434
Dale LaPlante <bmbmjmdm@gmail.com>
2024-12-15 15:29:07 +0100

    update model

--- src/store/Parameters.ts
+++ src/store/Parameters.ts
@@ -7,7 +7,8 @@ export const SIMILAR_LINES_TO_TITLES = 2;
 export const EMBEDDING_MODEL = "text-embedding-3-large"; 
 
 // can of course choose a stronger model if you like, but it costs more. gpt-4-turbo-preview, gpt-4-32k
-export const CHAT_MODEL = "gpt-3.5-turbo-0125";
+// gpt-3.5-turbo-0125 is what i was using, does this model name work? its supposedly cheaper and better
+export const CHAT_MODEL = "gpt-4o-mini";
 
 export const CHAT_MAX_TOKENS = 1000;
 export const CHAT_TEMPERATURE = 1;
